cc2c73ab1d595b9c1229803f5b4836230d73f86c
yhz <yhz18972165008@163.com>
2022-07-02 21:06:46 +0800

    登录界面

--- src/main/java/GUITest.java
+++ src/main/java/GUITest.java
@@ -1,36 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GUITest {
-    public static JTextField textField=new JTextField(20);
-    public static void main(String[] args){
-        JFrame frame=new JFrame("GUI example");
+    public static void main(String[] args) {
+        JFrame frame = new loginSpace("登录界面");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//窗口关闭退出程序
-        frame.setSize(400,500);//窗口大小
-        frame.setLayout(new BorderLayout());//设置边框布局管理器
-        JPanel panel =new JPanel();
-        //frame.setContentPane(panel);
-        frame.add(textField);
-        textField.setText("请输入 ");
-        JButton button =new JButton("");
-        frame.setLayout(null);
-        panel.add(button);
-        frame.add(panel);
-        panel.setBounds(0,0,100,200);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                get();
-            }
-        });
+        frame.setSize(500, 600);//设置大小
+        frame.setVisible(true);//显示窗口
 
-        frame.setBounds(800,300,400,500);
-        frame.setVisible(true);
-    }
-    public static void get(){
-        String str=textField.getText();
-        System.out.println(str);
     }
 }
+
--- src/main/java/excelWork.java
+++ src/main/java/excelWork.java
@@ -5,17 +5,18 @@ import java.util.List;
 public class excelWork {
     public static char separator=',';//分隔符
     public static  void main(String[] args) throws Exception{
-        String filePath="E:\\IntelliJ IDEA\\shortTerm\\GUI\\persons.csv";
+        String filePath_persons="E:\\IntelliJ IDEA\\shortTerm\\GUI\\persons.csv";//人员数据
+        String filepath_administrator="E:\\C++\\short_term\\administrator.csv";//管理员账户
         List<String[]> dataList= new ArrayList<String[]>();
-        //
-        //dataList.add(new String[]{"账号","姓名","年龄","性别","身份证号","行程记录","疫苗接种情况","健康码颜色","核酸情况"});
 
-    readCSV(filePath);
+    readPersonsCSV(filePath_persons);
+    dataList=readAdministrator(filepath_administrator);
+
     }
 
 
     //读取CSV文件
-    public static List<String[]> readCSV(String filePath) throws Exception {
+    public static List<String[]> readPersonsCSV(String filePath) throws Exception {
         CsvReader read = null;
         List<String[]> dataList = new ArrayList<String[]>();
         try {
@@ -52,4 +53,33 @@ public class excelWork {
     return dataList;
 
     }
+    public static List<String[]> readAdministrator(String filePath) throws Exception {
+        CsvReader read = null;
+        List<String[]> AdministratorList = new ArrayList<String[]>();//存放账户
+        try {
+            read = new CsvReader(filePath, separator, Charset.forName("GBK"));
+            //读表头
+            read.readHeaders();
+            //String[] headArray = read.getHeaders();
+            System.out.println(" ");
+            //读取数据
+            while (read.readRecord()) {
+                //System.out.println(read.getRawRecord());
+                //System.out.print(read.get(0)+"\t");
+                //AdministratorList.add(read.getValues());
+                //System.out.print(read.get(1)+"\t\n");
+                AdministratorList.add(read.getValues());
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (read != null) {
+                read.close();
+            }
+        }
+        return AdministratorList;
+}
+
 }
